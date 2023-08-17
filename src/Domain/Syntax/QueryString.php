61543d72e7d33e0b720c267983be43658d0d29bd
ChristophWolf <christoph.wolf@abacus.ch>
2023-08-17 08:55:16 +0200

    Allow setting fields option for QueryString

--- src/Domain/Syntax/QueryString.php
+++ src/Domain/Syntax/QueryString.php
@@ -17,25 +17,32 @@ class QueryString implements SyntaxInterface
 
     protected float $boost;
 
+    protected array $fields = [];
+
     protected string $defaultOperator;
 
-    public function __construct(string $queryString, string $defaultOperator = self::OP_OR, float $boost = 1.0)
+    public function __construct(string $queryString, string $defaultOperator = self::OP_OR, float $boost = 1.0, array $fields = [])
     {
         Assert::oneOf($defaultOperator, [self::OP_OR, self::OP_AND]);
 
         $this->queryString = $queryString;
         $this->boost = $boost;
         $this->defaultOperator = $defaultOperator;
+        $this->fields = $fields;
     }
 
     public function build(): array
     {
-        return [
+        $query = [
             'query_string' => [
                 'query' => $this->queryString,
                 'default_operator' => $this->defaultOperator,
                 'boost' => $this->boost,
             ],
         ];
+        if (!empty($this->fields)) {
+            $query['query_string']['fields'] = $this->fields;
+        }
+        return $query;
     }
 }
